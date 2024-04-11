0ed3d59578b43d977d61179ba5faaae03b2d59de
Wojciech_Malicki <wojciech.malicki09@gmail.com>
2024-04-11 14:34:21 +0200

    initial commit

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,4 +1,10 @@
 describe('Swiper Gallery Test', function () {
+  it('Checks if first slide contains "Rome"', function () {
+    cy.visit('http://localhost:3000');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
@@ -10,9 +16,37 @@ describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
-    cy.wait(2000);
+    cy.wait(2000)
     cy.get('.swiper-button-next').click({ force: true });
-    cy.wait(2000);
+    cy.wait(2000)
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if slide looping works', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if slide backtracking works', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-button-prev').click({ force: true });
+    cy.wait(2000)
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
